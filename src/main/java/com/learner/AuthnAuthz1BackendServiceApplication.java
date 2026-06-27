edcb49369cca74e7efcde84efe0a879c2746e51a
Anupam Kumar <anupam.nitdgp14@gmail.com>
2026-06-27 20:14:25 +0530

    Project Initialization.

---
+++ src/main/java/com/learner/AuthnAuthz1BackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthnAuthz1BackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthnAuthz1BackendServiceApplication.class, args);
+	}
+
+}
